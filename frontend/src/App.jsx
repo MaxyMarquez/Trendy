702c094d610de318d418ba59680744e6c4895807
Maxy Marquez <maxy.marquez@hotmail.com>
2023-11-23 17:51:18 -0300

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <Home />
+      <a href="/home">Home</a>
     </>
   )
 }
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -12,7 +12,6 @@ const Navbar = () => {
     return (
         <>
             <a href="/home">Home</a>
-
         </>
     )
 }
