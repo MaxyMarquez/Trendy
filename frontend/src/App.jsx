3c87bcfc01dc4e11893c9610fd28839bd1e10b8f
Maxy Marquez <maxy.marquez@hotmail.com>
2023-11-23 18:00:09 -0300

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,13 @@ import axios from 'axios'
 axios.defaults.baseURL = 'http://localhost:3002/api'
 import './App.css'
 import Home from './components/Home/Home'
-import { Route, Router, Routes } from 'react-router'
+import { Route, Routes } from 'react-router'
 
 function App() {
 
   return (
     <>
-      <a href="https://trendy-4f2i.vercel.app/home">HOME</a>
+      <a href="/home">HOME</a>
 
       <Routes>
         <Route path='/home' element={<Home />} />
